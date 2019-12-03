6cba6dec09437ba0415221ff528b57b8f48053a8
BaeJaejin <BaeJaejin@180.64.66.226>
2019-12-04 01:00:37 +0900

    수정

--- src/main/webapp/resources/js/m/common.js
+++ src/main/webapp/resources/js/m/common.js
@@ -15,3 +15,41 @@ $(function(){
 		$(this).find(".lnb-wrap").slideToggle();
 	});
 });
+
+function advice_register(info){
+	$.ajax({
+		url:"/bestsolution/adviceRegister",
+		type: "post",
+		data:JSON.stringify(info),
+		async:false,
+		contentType : "application/json; charset=UTF-8",
+		dataType:"text",
+		success:function(json){
+			if(json == "ok"){
+				alert("문의사항이 접수 되었습니다.\n빠른 시일 내 연락드리겠습니다.\n감사합니다.");
+			}else{
+				alert("문의글 등록이 정상적으로 등록되지 않았습니다. 새로고침(F5) 후 다시 이용하세요.");
+			}
+		},
+		error:function(request,status,error){
+			console.log("code:"+request.status+"\n"+"message:"+request.responseText+"\n"+"error:"+error);
+		}
+	});
+}
+
+function statisticRegister(info){
+	$.ajax({
+		url:"/bestsolution/statisticRegister",
+		type:"post",
+		data:JSON.stringify(info),
+		contentType : "application/json; charset=UTF-8",
+		dataType:"text",
+		async:false,
+		success:function(json){
+			console.log(json);
+		},
+		error:function(request,status,error){
+			console.log("code:"+request.status+"\n"+"message:"+request.responseText+"\n"+"error:"+error);
+		}
+	});
+}
